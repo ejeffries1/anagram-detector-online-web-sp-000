f9ed70d95ff252f3cbe8954506737b79e99d065a
Evan Jeffries <ejeffries14@gmail.com>
2021-06-04 02:37:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    #detector = self.new(word)
+  end
+
+  def match(array)
+    new_word = @word.split("")
+    array.each do |el|
+      new_el = el.split("")
+      binding.pry
+      if new_word.sort != new_el.sort
+        []
+      end
+    end
+  end
+end
