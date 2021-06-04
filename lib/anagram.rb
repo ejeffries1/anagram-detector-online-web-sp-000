7e03398cb4a565ecc4f6925295ee477848b31b51
Evan Jeffries <ejeffries14@gmail.com>
2021-06-04 02:56:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     new_word = @word.split("")
     array.each do |el|
       new_el = el.split("")
-      if new_el.sort == new_word.sort == true
+      if new_el.sort == new_word.sort = true
         new_el
       end
     end
